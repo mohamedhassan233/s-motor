be1377904358c75d203eea41c03106857d10cde8
Mohamed Hassan <47718448+mohamedhassan233@users.noreply.github.com>
2022-01-05 17:40:15 +0300

    Add files via upload

---
+++ anti_45.asm
@@ -0,0 +1,14 @@
+.MODEL SMALL
+.DATA
+    
+    ; bin data for anti clock-wise
+    data_acw  DB 0000_0011B
+              	   DB 0000_0001B    
+              	   DB 0000_0110B
+              	   DB 0000_0010B        
+    
+
+
+
+
+
