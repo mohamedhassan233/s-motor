767015840eead6639ed24380532658780d1d97cd
Bassam Salama <84365449+DevBassam@users.noreply.github.com>
2022-01-05 16:34:28 +0200

    Update anti_180.asm

--- anti_180.asm
+++ anti_180.asm
@@ -4,7 +4,7 @@
 .DATA
     
     ; bin data for anti clock-wise
-    data_acw  DB 0000_0011B
+    data_acw       DB 0000_0011B
               	   DB 0000_0001B    
               	   DB 0000_0110B
               	   DB 0000_0010B        
