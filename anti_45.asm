3faa188ce6db72eb6f816091815485237205c247
Mohamed Hassan <47718448+mohamedhassan233@users.noreply.github.com>
2022-01-07 15:46:13 +0300

    Update anti_45.asm

--- anti_45.asm
+++ anti_45.asm
@@ -20,3 +20,10 @@ next_step:
 
 MOV AL, [BX][SI] ; accessing the array by index
 OUT 7, AL ; update stepper motors's bins by the value at AL
+INC SI
+INC CX
+CMP SI, 4      
+JB next_step
+MOV SI,0
+CMP CX ,4     ; '4' for 45-degree rotation
+JB  next_step
